dde9b49836d644a197d44fece6be5eac22806be3
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-09-30 15:40:16 +0000

    Minor changes

--- debug-args.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-console.log('process.argv[0]:', process.argv[0]);
-console.log('process.argv[1]:', process.argv[1]);
-console.log('process.env._:', process.env._);
-console.log('path.basename(process.argv[1]):', require('path').basename(process.argv[1]));
-console.log('All args:', process.argv);
--- test-args.js
+++
@@ -1,1 +0,0 @@
-console.log("argv[0]:", process.argv[0]); console.log("argv[1]:", process.argv[1]); console.log("env._:", process.env._); console.log("basename argv[1]:", require("path").basename(process.argv[1]));
